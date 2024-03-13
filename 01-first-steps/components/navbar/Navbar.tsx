9c2fae5998a7fe024cbf18033587c2b843d9a5d8
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-03-13 15:29:15 -0600

    usePathname - ActiveLink

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { HomeIcon } from "@primer/octicons-react"
 import Link from "next/link"
+import { ActiveLink } from ".."
 
 const navItems = [
   {path: '/about', text: 'About'},
@@ -18,7 +19,7 @@ export const Navbar = () => {
         <div className="flex flex-1"></div>
 
         {navItems.map(navItem => (
-          <Link key={navItem.path} className="mr-2" href={navItem.path}>{navItem.text}</Link>
+          <ActiveLink key={navItem.path} {...navItem} />
         ))}
     </nav>
   )
