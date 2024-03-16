acf8bc06b5aacb1b73e85e75985bb647f0b48623
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-03-15 18:54:07 -0600

    Cosumir API pokemon, por ID de pokemon

--- my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
+++ my-dashboard/src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,13 +1,26 @@
+import { Pokemon } from "@/pokemons";
+
 interface Props {
     params: { id: string}
 }
-export default function PokemonPage({params}: Props) {
 
-    const {id} = params;
+const getPokemon = async(id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,{
+        cache: 'force-cache' // TODO: cambiar despues
+    }).then(resp => resp.json())
+
+    return pokemon;
+}
+export default async function PokemonPage({params}: Props) {
+
+    const pokemon = await getPokemon(params.id);
 
     return (
         <div>
-            <h1>Pagina del pokemon {id}</h1>
+            <h1>Pagina del pokemon {params.id}</h1>
+            <div>
+                {JSON.stringify(pokemon)}
+            </div>
         </div>
     );
 }
--- my-dashboard/src/pokemons/index.ts
+++ my-dashboard/src/pokemons/index.ts
@@ -1,6 +1,7 @@
 // Interfaces
 export type {PokemonsResponse, Result} from './interfaces/pokemons-response';
 export type {SimplePokemon} from './interfaces/simple-pokemon';
+export type {Pokemon} from './interfaces/pokemon';
 
 // Componentes
 export {PokemonGrid} from './components/PokemonGrid';
