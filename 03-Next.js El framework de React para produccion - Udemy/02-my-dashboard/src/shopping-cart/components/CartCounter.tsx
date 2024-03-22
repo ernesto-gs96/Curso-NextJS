da0bf97986e56ad8c00b7dae447d07d030fe9871
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-03-21 18:46:03 -0600

    Implementacion de Redux para el counter

--- 03-Next.js El framework de React para produccion - Udemy/02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
+++ 03-Next.js El framework de React para produccion - Udemy/02-my-dashboard/src/shopping-cart/components/CartCounter.tsx
@@ -1,26 +1,39 @@
 "use client";
 
-import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/store";
+import { initCounterState, addOne, subStractOne } from "@/store/counter/counterSlice";
+import { useEffect, useState } from "react";
 
 interface Props {
-    value?: number
+  value?: number
 }
 
-export const CartCounter = ({value = 10} : Props) => {
+export const CartCounter = ({ value = 10 }: Props) => {
 
-    const [count, setCount] = useState(value);
+  const count = useAppSelector(state => state.counter.count);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initCounterState(value))
+  }, [dispatch, value])
+
+  // const [count, setCount] = useState(value);
   return (
     <>
-        <span className="text-9xl">{count}</span>
+      <span className="text-9xl">{count}</span>
       <div className="flex">
-        <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCount(count + 1)}
+        <button 
+          type="button"
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => dispatch(addOne())}
         >
           +1
         </button>
 
-        <button className="flex items-center justify-center p-2 rounded-xl bg-red-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
-          onClick={() => setCount(count - 1)}
+        <button 
+          type="button"
+          className="flex items-center justify-center p-2 rounded-xl bg-red-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2"
+          onClick={() => dispatch(subStractOne())}
         >
           -1
         </button>
