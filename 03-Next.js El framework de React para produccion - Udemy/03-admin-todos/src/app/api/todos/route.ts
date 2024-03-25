1327c6f58c02e4d3088ac129eff21faf223257b3
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-03-24 23:18:16 -0600

    API REST: Registro todo con validaciones de atributos (se puede mejorar)

--- 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/app/api/todos/route.ts
+++ 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/app/api/todos/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/app/lib/prima";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
 
@@ -24,11 +25,18 @@ export async function GET(request: Request) {
     return NextResponse.json(todos);
 }
 
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false),
+})
+
 export async function POST(request: Request) {
-    
-    const body = await request.json();
-
-    const todo = await prisma.todo.create({ data: body })
-
-    return NextResponse.json(todo);
+    try {
+        // TODO: Mejorar para mandar error si envio atributos extras
+        const { description, complete } = await postSchema.validate( await request.json() );
+        const todo = await prisma.todo.create({ data: { description, complete } });
+        return NextResponse.json(todo);
+    } catch (error) {
+        return NextResponse.json( error, { status: 400 } );
+    }
 }
