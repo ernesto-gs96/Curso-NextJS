607d8ee732ff2e6c79c09ce864540808818273e1
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-03-24 23:24:39 -0600

    API REST: Actualizacion de registro sin validaciones

--- 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/app/api/todos/[id]/route.ts
+++ 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -23,3 +23,27 @@ export async function GET(request: Request, { params }: Segments) {
 
     return NextResponse.json(todo)
 }
+
+export async function PUT(request: Request, { params }: Segments) {
+
+    const { id } = params;
+
+    const todo = await prisma.todo.findUnique({
+        where: {
+            id: id
+        }
+    });
+
+    if ( !todo ) {
+        return NextResponse.json({ message: `todo con ${id} no existe` }, { status: 404 } )
+    }
+
+    const body = await request.json();
+
+    const updateTodo = await prisma.todo.update({
+        where: { id },
+        data: { ...body }
+    });
+
+    return NextResponse.json(updateTodo)
+}
