0f5338494ede0795d8e897aaafd1298a7e9d3844
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-06 16:11:20 -0600

    Agregar, eliminar y mostrar total de productos agregados

--- 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/products/components/ProductCard.tsx
+++ 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/products/components/ProductCard.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 // https://tailwindcomponents.com/component/e-commerce-product-card
 
 import Image from "next/image"
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5"
 import { Star } from "./Star";
+import { addProductToCart, removeProductToCart } from "@/shopping-cart/actions/actions";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id    : string;
@@ -13,6 +17,19 @@ interface Props {
 }
 
 export const ProductCard = ( { id, name, price, rating, image }:Props) => {
+
+  const router = useRouter();
+
+  const onAddToCart = () => {
+    addProductToCart(id);
+    router.refresh();
+  }
+
+  const onRemoveToCart = () => {
+    removeProductToCart(id);
+    router.refresh();
+  }
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       
@@ -50,16 +67,18 @@ export const ProductCard = ( { id, name, price, rating, image }:Props) => {
 
         {/* Price and Add to Cart */}
         <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">{`$ ${price}`}</span>
+          <span className="text-xl font-bold text-gray-900 dark:text-white">${price.toFixed(2)}</span>
           
           <div className="flex">
             <button
+              onClick={onAddToCart}
               type="button"
               title="Agregar"
               className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 <IoAddCircleOutline size={25} />
             </button>
             <button
+              onClick={onRemoveToCart}
               type="button"
               title="Borrar"
               className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
---
+++ 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/shopping-cart/actions/actions.ts
@@ -0,0 +1,26 @@
+import { getCookie, hasCookie, setCookie } from "cookies-next"
+
+export const getCookieCart = ():{ [ id:string ]:number } => {
+    
+    let cookieCart = {};
+    if( hasCookie('cart') ){
+        cookieCart = JSON.parse( getCookie('cart') as string ?? '{}' );
+    }
+    return cookieCart;
+}
+
+export const addProductToCart = ( id:string ) => {
+    const cookieCart = getCookieCart();
+    if( cookieCart[id] ){
+        cookieCart[id] = cookieCart[id] + 1;
+    } else {
+        cookieCart[id] = 1;
+    }
+    setCookie('cart', JSON.stringify(cookieCart));
+}
+
+export const removeProductToCart = ( id:string ) => {
+    const cookieCart = getCookieCart();
+    delete cookieCart[id];
+    setCookie('cart', JSON.stringify(cookieCart));
+}
