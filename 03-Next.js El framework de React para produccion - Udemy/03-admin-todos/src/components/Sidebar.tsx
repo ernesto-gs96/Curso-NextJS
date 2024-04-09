dafa8b37b2a11f3422c98eb4ac3dafdfc0b439a2
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-08 19:49:56 -0600

    authjs con provider github y utilizacion de informacion de session en servidor y cliente

--- 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/components/Sidebar.tsx
+++ 03-Next.js El framework de React para produccion - Udemy/03-admin-todos/src/components/Sidebar.tsx
@@ -3,7 +3,9 @@ import Link from "next/link"
 
 import { CiBookmarkCheck, CiLogout } from 'react-icons/ci';
 import { SidebarItem } from "./SidebarItem";
-import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from "react-icons/io5";
+import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline, IoPerson, IoPersonOutline } from "react-icons/io5";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const menuItems = [
     { path: "/dashboard", icono: <IoCalendarOutline size={30} />, title: "Dasboard" },
@@ -11,9 +13,16 @@ const menuItems = [
     { path: "/dashboard/server-todos", icono: <IoListOutline size={30} />, title: "Server Actions" },
     { path: "/dashboard/cookies", icono: <IoCodeWorkingOutline size={30} />, title: "Cookies" },
     { path: "/dashboard/products", icono: <IoBasketOutline size={30} />, title: "Productos" },
+    { path: "/dashboard/profile", icono: <IoPersonOutline size={30} />, title: "Perfil" },
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+
+    const session = await getServerSession(authOptions);
+    const name = session?.user?.name ?? 'Not name';
+    const image = session?.user?.image ?? "https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp";
+    
+
     return (
         <>
             <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -33,14 +42,14 @@ export const Sidebar = () => {
 
                     <div className="mt-8 text-center">
                         <Image
-                            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+                            src={image}
                             alt=""
                             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
                             width={100}
                             height={100}
                             priority
                         />
-                        <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+                        <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{name}</h5>
                         <span className="hidden text-gray-400 lg:block">Admin</span>
                     </div>
 
