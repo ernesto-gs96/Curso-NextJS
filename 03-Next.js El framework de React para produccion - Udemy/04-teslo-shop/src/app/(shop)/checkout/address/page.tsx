fddd8d565675ef5afec59b8a4e8d7c4ed0bb807f
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-13 22:58:32 -0600

    Rutas de la aplicacion

---
+++ 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/app/(shop)/checkout/address/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CheckOutAddressPage() {
+  return (
+    <div>
+      <h1>CheckOutAddress Page</h1>
+    </div>
+  );
+}
---
+++ 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/app/(shop)/products/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ProdutsPage() {
+  return (
+    <div>
+      <h1>Produts Page</h1>
+    </div>
+  );
+}
