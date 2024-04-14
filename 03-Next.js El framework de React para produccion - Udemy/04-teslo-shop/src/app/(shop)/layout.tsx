3848c6ae160ea0444b18c8eaf11c6e05b4728df5
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-13 23:39:26 -0600

    TopMenu

--- 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/app/(shop)/layout.tsx
+++ 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/app/(shop)/layout.tsx
@@ -1,10 +1,12 @@
+import { TopMenu } from "@/components";
 
 export default function ShopLayout({ children }: {
  children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-red-500">
-        {children}
+    <div className="min-h-scree">
+      <TopMenu/>
+      {children}
     </div>
   );
 }
