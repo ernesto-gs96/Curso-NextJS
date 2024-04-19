46fc45c31f2550a7799fea5a5d4c604fbf2fbeaf
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-18 19:41:56 -0600

    Zustand UI Store

--- 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/components/ui/top-menu/TopMenu.tsx
+++ 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/components/ui/top-menu/TopMenu.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import { titleFont } from "@/config/fonts"
+import { useUIStore } from "@/store"
 import Link from "next/link"
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5"
 
 export const TopMenu = () => {
+
+    const openMenu = useUIStore( state => state.openSideMenu)
   return (
     <nav className="flex px-5 justify-between items-center w-full">
         {/* Logo */}
@@ -53,7 +58,10 @@ export const TopMenu = () => {
                 </div>
             </Link>
 
-            <button className="m-2 p-2 rounded-md transition-all hover:bg-gray-100">
+            <button 
+                className="m-2 p-2 rounded-md transition-all hover:bg-gray-100"
+                onClick={openMenu}
+            >
                 Menú
             </button>
         </div>
