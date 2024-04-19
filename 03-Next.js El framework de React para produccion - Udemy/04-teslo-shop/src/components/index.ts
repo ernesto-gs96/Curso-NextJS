61462cbe723d9ba00f9c08dacb0334af7ff858e8
Cirilo Ernesto García Soriano <ernesto_gs96@hotmail.com>
2024-04-18 19:14:21 -0600

    menu lateral

--- 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/components/index.ts
+++ 03-Next.js El framework de React para produccion - Udemy/04-teslo-shop/src/components/index.ts
@@ -1,4 +1,14 @@
+// cart
+
+
+// product
+
+
+// products
+export * from './products/product-grid/ProductGrid';
+
+// ui
 export *  from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title'
-export * from './products/product-grid/ProductGrid';
+export * from './ui/sidebar/Sidebar';
